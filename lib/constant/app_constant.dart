45db4aac921a0f628794f3f1c39a9f58a24d7266
Yazid Bintang <yazidbintang2711@gmail.com>
2024-04-23 11:11:08 +0700

    Add API URL

---
+++ lib/constant/app_constant.dart
@@ -0,0 +1,7 @@
+class AppConstant {
+  static const API_KEY = "f7af57eee03f8a2ad9f1528179edce3e";
+  static const TRENDING_MOVIES =
+      "https://api.themoviedb.org/3/trending/movie/week?api_key=$API_KEY";
+  static const POPULAR_MOVIES =
+      "https://api.themoviedb.org/3/movie/popular?api_key=$API_KEY";
+}
