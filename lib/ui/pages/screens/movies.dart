dd7401614449c9c427c0f330fc5203f50cd797ac
Yazid Bintang <yazidbintang2711@gmail.com>
2024-04-28 15:35:51 +0700

    Create movies screen

---
+++ lib/ui/pages/screens/movies.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:moviestar/controller/app_controller.dart';
+import 'package:moviestar/ui/widgets/movie_list.dart';
+
+class ListScreen extends StatefulWidget {
+  const ListScreen({super.key});
+
+  @override
+  State<ListScreen> createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var controller = Get.put(AppController());
+    return SingleChildScrollView(
+      child: Expanded(
+        child: Obx(() => controller.loading.value
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Column(
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height / 14,
+                  ),
+                  MovieListWidget(
+                      category: "Top Rated",
+                      data: controller.getTopRatedMovies),
+                  MovieListWidget(
+                      category: "Popular", data: controller.getPopularMovies),
+                  MovieListWidget(
+                      category: "Trending", data: controller.getTrendingMovies),
+                  MovieListWidget(
+                      category: "Now Playing",
+                      data: controller.getNowPlayingMovies)
+                ],
+              )),
+      ),
+    );
+  }
+}
